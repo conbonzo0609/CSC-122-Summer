26615df16de238a672b79473ceee280ff3352daf
Connor Smith <conbonzo0609@gmail.com>
2020-05-23 14:19:13 -0500

    Start of first project

---
+++ Portfolio1/inputprotection.h
@@ -0,0 +1,13 @@
+#ifndef inputprotection_h
+#define inputprotection_h
+
+#include <string>
+
+using namespace std; 
+
+
+
+
+
+
+#endif
---
+++ Portfolio1/protinplib/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "inputprotection.h"
+#include <cstring>
+
+using namespace std; 
+
+
+
+int main(void){
+
+
+
+
+
+
+
+
+ 
+
+
+}
