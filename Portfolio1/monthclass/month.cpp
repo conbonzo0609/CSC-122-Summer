05919fe151a20a516ecc745bac6bf1ecec1b2fdf
Connor Smith <conbonzo0609@gmail.com>
2020-06-03 17:17:27 -0500

    Patched set_month

--- Portfolio1/monthclass/month.cpp
+++ Portfolio1/monthclass/month.cpp
@@ -1,19 +1,53 @@
 
 #include <iostream>
 #include "month.h"
-
+#include <string>
+#include <unordered_map>
 
 using namespace std; 
 
+std:: unordered_map<string, int> look_up = {
+        { "jan", 1 },
+        { "feb", 2 }, 
+        { "mar", 3 }, 
+        { "apr", 4 }, 
+        { "may", 5 },
+        { "jun", 6 }, 
+        { "jul", 7 }, 
+        { "aug", 8 }, 
+        { "sept",9},
+        {"oct", 10},
+        {"nov", 11},
+        {"dec", 12}
+
+};
+
 
 bool Month::set_month () {
-int x; 
-    cout << "Please enter the Month";
+string x,y,substring,lowercase; 
+int myNumbered;
 
-    cin >> x; 
+cout << "Please enter the Month";
 
-    if(x>0 & x <=12){
-        myMonth = x; 
+cin >> x; 
+
+for(string::size_type a = 0; a<x.length(); a++){
+    if(isdigit (x[a])){
+        y += x[a];
+    }else{
+        lowercase += tolower(x[a]);
+    }
+}
+
+if(y.length() > 0){
+     myNumbered = stoi(y);
+}else{
+    substring = lowercase.substr(0,3);
+    myNumbered = look_up[substring];
+    }
+
+    if(myNumbered>0 & myNumbered<=12){
+        myMonth = myNumbered; 
         return true;
     }else{
         return false; 
